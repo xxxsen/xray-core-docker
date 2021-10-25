8eaa10efcafe22dfa25d3b1b0999e49268431944
xxxtest <xxxsen@example.com>
2021-10-25 22:59:27 +0800

    push latest image

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ docker buildx create --use --name mybuild
 
 docker buildx build --build-arg version=${VERSION} -t xxxsen/xray-core:${VERSION} \
   --platform=linux/amd64,linux/arm64,linux/ppc64le,linux/386,linux/arm/v7,linux/arm/v6 . --push
+docker buildx build --build-arg version=${VERSION} -t xxxsen/xray-core:latest \
+  --platform=linux/amd64,linux/arm64,linux/ppc64le,linux/386,linux/arm/v7,linux/arm/v6 . --push
