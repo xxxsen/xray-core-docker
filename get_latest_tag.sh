95ea5abf6e4e9aaf95aca97fd10c07a55ee1f13d
xxxtest <xxxsen@example.com>
2021-11-03 21:27:10 +0800

    update build

--- get_latest_tag.sh
+++ get_latest_tag.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tagname=$(curl "https://github.com/XTLS/Xray-core/releases" 2>&1 | grep "tree" | grep "data-view-component"  | head -n 1 | awk -F"\"" '{print $2}' | awk -F"/" '{print $5}')
+tagname=$(curl "https://github.com/$1/releases" 2>&1 | grep "tree" | grep "data-view-component"  | head -n 1 | awk -F"\"" '{print $2}' | awk -F"/" '{print $5}')
 echo "::set-output name=LATEST_TAG_NAME::${tagname}"
